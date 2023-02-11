046b9f32be9e0421b0b0878714c6495976d71648
комп класс А105 <a105@pspu.ru>
2023-02-11 12:13:46 +0500

    My Django Girls app, first commit

---
+++ mysite/token.py
@@ -0,0 +1,15 @@
+import requests
+username = 'denox'
+token = '1a08089d119d26702127fbac5a3d1a62266fab11'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
